a91794df6ea900abdb6fc2f6befbbc0bbb127dc7
ten <yuptyy@163.com>
2018-04-18 08:30:22 +0800

    清理

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -3,17 +3,12 @@
     <h1 class='h1'>{{msg}}</h1>
 
     <child msg='from index parent' />
-
-    <a href="/me">关于我</a>    
-    <a href="/discover">探索</a>
   </div>
 </template>
 
 
 <script>
 import child from './child/child'
-// import _ from 'lodash'
-
 
 export default {
 	components: {
--- src/pages/module/sub-module/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1 class="h1">submodule</h1>
-</template>
-
-<style scoped lang="scss">
-  .h1 {
-    color: yellow;
-  }
-</style>
